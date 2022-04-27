86c6446983fa5ad1355700c17a66d1c8b42c20f9
pushkardangi6350 <94898640+pushkardangi6350@users.noreply.github.com>
2022-04-27 15:16:49 +0530

    Create bloglist.js

---
+++ js/bloglist.js
@@ -0,0 +1,25 @@
+var modal3 = document.getElementById("deleteId");
+// Get the button that opens the modal
+var btn31 = document.getElementsByClassName("abc1");
+// Get the <span> element that closes the modal
+var span3 = document.getElementById("na");
+
+btn31[0].onclick=function() {
+  modal3.style.display = "block";
+ }
+btn31[1].onclick=function() {
+ modal3.style.display = "block";
+}
+btn31[2].onclick=function() {
+ modal3.style.display = "block";
+}
+btn31[3].onclick=function() {
+ modal3.style.display = "block";
+}
+btn31[4].onclick=function() {
+ modal3.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
